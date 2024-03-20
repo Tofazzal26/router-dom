87ba065a51ed51504c95f0da825ee10be49dd2ec
Tofazzal26 <afranislamabir6789@gmail.com>
2024-03-20 18:54:43 +0600

    loading validation finish

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,15 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
+  const navigation = useNavigation();
+
   return (
     <div>
       <Header />
       <h2 className="text-3xl bg-[#ffbd45] text-center p-2 my-2 text-white">
         well come to home
       </h2>
-      <Outlet />
+
+      {navigation.state === "loading" ? (
+        <p className="text-4xl text-center text-blue-600">Loading...</p>
+      ) : (
+        <Outlet />
+      )}
       <Footer />
     </div>
   );

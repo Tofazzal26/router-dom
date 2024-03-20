203f17e4013e1bbf9a94f01d31e5d585c065175e
Tofazzal26 <afranislamabir6789@gmail.com>
2024-03-20 16:41:02 +0600

    details and go back validation finish

--- src/Components/UserDetails/UserDetails.jsx
+++ src/Components/UserDetails/UserDetails.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const UserDetails = () => {
+  const details = useLoaderData();
+  const { name, email, address } = details;
+
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <div>
-      <h2>this user details page</h2>
+    <div className="text-center text-xl">
+      <h2>{name}</h2>
+      <h2>{email}</h2>
+      <h2>{address.street}</h2>
+      <button
+        onClick={handleBack}
+        className="bg-red-500 text-white py-2 px-5 rounded-md"
+      >
+        Go Back
+      </button>
     </div>
   );
 };
